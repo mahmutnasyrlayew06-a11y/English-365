15c66702c9459b3f439c585cfd55797f2a93fa38
mahmutnasyrlayew06-a11y <mahmutnasyrlayew06@gmail.com>
2026-07-31 20:09:31 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+// service-worker.js
+const CACHE_NAME = 'english365-v1';
+const urlsToCache = [
+    'index.html',
+    'style.css',
+    'script.js',
+    'data.js',
+    'manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(name) {
+                    return name !== CACHE_NAME;
+                }).map(function(name) {
+                    return caches.delete(name);
+                })
+            );
+        })
+    );
+});
